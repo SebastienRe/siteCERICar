f3c3bcee99fd8d4d7e5bf0352e521a0e8c92e24f
Sébastien Ré <sebfabien13@gmail.com>
2022-10-18 17:09:23 +0200

    Implementation des classes

---
+++ CERICAR/view/indexSuccess.php
@@ -0,0 +1,2 @@
+C'est l'action par défaut ! 
+<a href=index.php?action=logout>Deconnectez vous !</a>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+//nom de l'application
+$nameApp = "CERICAR";
+
+//action par défaut
+$action = "index";
+
+if(key_exists("action", $_REQUEST))# si action est dans $_request
+	$action =  $_REQUEST['action'];
+
+require_once 'lib/core.php';
+require_once $nameApp.'/controller/mainController.php';#on inclut le controlleur
+
+foreach(glob($nameApp.'/model/*.class.php') as $model) #pour chaque model
+	include_once $model ; # l'inclure
+
+session_start();
+
+$context = context::getInstance();
+$context->init($nameApp);
+
+$view=$context->executeAction($action, $_REQUEST);
+
+//traitement des erreurs de bases, reste a traiter les erreurs d'inclusion
+if($view===false)
+{
+	echo "Une grave erreur s'est produite, il est probable que l'action ".$action." n'existe pas...";
+	die;
+}
+//inclusion du layout qui va lui meme inclure le template view
+elseif($view!=context::NONE)
+{
+	$template_view=$nameApp."/view/".$action.$view.".php";
+	include($nameApp."/layout/".$context->getLayout().".php");
+}
+
+?>
